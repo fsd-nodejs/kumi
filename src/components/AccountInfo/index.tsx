864c239a26b4d735305d77da67496581d600aee1
Vern Zhang <vern.zhang@crypto.com>
2023-02-08 20:02:21 +0800

    chore: update wallet list UIs

--- src/components/AccountInfo/index.tsx
+++ src/components/AccountInfo/index.tsx
@@ -1,5 +1,5 @@
 import Icon from '@polkadot/react-identicon'
-import { Avatar, Card, Typography } from 'antd'
+import { Avatar, Badge, Card, Typography } from 'antd'
 import { FC } from 'react'
 
 const { Paragraph } = Typography
@@ -11,19 +11,27 @@ export const AccountInfo: FC<{ name?: string; address?: string }> = ({
   const cName = name ?? ''
   const cAddress = address ?? ''
   return (
-    <Card style={{ marginBottom: 16 }}>
-      <Card.Meta
-        avatar={
-          <Avatar icon={<Icon size={32} value={cAddress || 'User'} />}></Avatar>
-        }
-        title={cName.length > 2 ? name : '<unknown>'}
-      />
-      <Paragraph
-        copyable={{ tooltips: ['Copy address', 'Copied!'] }}
-        style={{ marginTop: 16, marginBottom: 0 }}
-      >
-        {cAddress || '<unknown>'}
-      </Paragraph>
-    </Card>
+    <Badge.Ribbon
+      text="Calamari Parachain Staging"
+      color="#ff7d01"
+      style={{ fontSize: '12px', lineHeight: '18px', height: '18px' }}
+    >
+      <Card style={{ marginBottom: 16 }}>
+        <Card.Meta
+          avatar={
+            <Avatar
+              icon={<Icon size={32} value={cAddress || 'User'} />}
+            ></Avatar>
+          }
+          title={cName.length > 2 ? name : '<unknown>'}
+        />
+        <Paragraph
+          copyable={{ tooltips: ['Copy address', 'Copied!'] }}
+          style={{ marginTop: 16, marginBottom: 0 }}
+        >
+          {cAddress || '<unknown>'}
+        </Paragraph>
+      </Card>
+    </Badge.Ribbon>
   )
 }
